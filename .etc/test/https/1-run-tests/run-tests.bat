f710a2177a38708d2b1321c375f90c993caace03
Warren R. Bank <warren.r.bank@gmail.com>
2026-03-15 05:22:25 -0700

    update tests

--- .etc/test/https/1-run-tests/run-tests.bat
+++ .etc/test/https/1-run-tests/run-tests.bat
@@ -3,9 +3,9 @@
 set CURL_HOME=C:\PortableApps\curl
 set PATH=%CURL_HOME%;%PATH%
 
-set tests="%~dp0..\tests.bat"
+set tests="%~dp0..\..\tests.bat"
 set log="%~dpn0.log.txt"
 
-set serve_url=http://127.0.0.1:80
+set serve_url=https://127.0.0.1:443
 
 call %tests% >%log% 2>&1
